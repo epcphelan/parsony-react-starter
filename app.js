6cf23edfe9c762f9b3b0519f05688efaf7cb21fe
Eric Phelan <epcphelan@gmail.com>
2018-07-08 01:15:56 +0200

    fixed require

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const configs = require('./config.json');
 const {join} = require('path');
 configs.static_files = join(__dirname,configs.static_files);
 
-const ParsonyWebServer = require('/Users/ericphelan/Desktop/NPM/parsony-web');
+const ParsonyWebServer = require('/Users/ericphelan/Desktop/ParsonyFramework/parsony-web');
 const parsony = new ParsonyWebServer(configs);
 parsony.start();
